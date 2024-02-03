3eea920e8d2963213a86eeeecc3b1f314c5578f9
Catherine Loesch <catherine.loesch@protonmail.com>
2024-02-03 21:04:12 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+// Matrix canvas variables
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+const fontSize = 16;
+const columns = Math.floor(windowWidth / fontSize);
+var drops = [];
+
+for (let i=0; i<columns; i++) {
+    drops.push(0);
+
+}
+
+let str = "This is your last chance After this there is no turning back You take the blue pill the story ends you wake up in your bed and believe whatever you want to believe You take the red pill you stay in Wonderland and I show you how deep the rabbit hole goes 你服下蓝色药丸故事就结束了你在床上醒来相信你想相信的切服下红色药丸你留在仙境我告诉你兔子洞有多深 あなたは赤い薬を飲むとワンダランドに留まるそして私はウサギの穴がどれほど深いかを教えてあげる";
+
+const draw = () => {
+    context.fillStyle = "rgba(0, 0, 0, 0.05)";
+    context.fillRect(0, 0, windowWidth, windowHeight);
+    context.fontSize = "700 " + fontSize + "px";
+    context.fillStyle = "#00cc33";
+
+    for (let i=0; i<columns; i++) {
+
+        let index = Math.floor(Math.random()*str.length);
+        let x = i * fontSize;
+        let y = drops[i] * fontSize;
+
+        context.fillText(str[index], x, y);
+
+        if (y >= canvas.height && Math.random() > 0.99) {
+            drops[i] = 0;
+        }
+
+        drops[i]++;
+
+    }
+}
+
+draw();
+setInterval(draw, 35);
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Resources & tutorials
+
+- [Hacking Matrix Rain Effect using HTML CSS & JavaScript](https://www.youtube.com/watch?v=uIq9gmlenlA&list=PL5TCQHRtvHGkuR2YZDf_2lDFuc9m5OUlO&index=22) by [CodeCreative](https://www.youtube.com/@CodeCreativeYT)
